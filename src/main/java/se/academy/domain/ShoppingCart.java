fcc84851ffd468aae7f1973dea3ff641c42d7b58
Hasseoa <hasseoa@gmail.com>
2017-12-09 12:16:36 +0100

    add HashMap to shoppingcart and copypasted html

--- src/main/java/se/academy/domain/ShoppingCart.java
+++ src/main/java/se/academy/domain/ShoppingCart.java
@@ -1,21 +1,18 @@
 package se.academy.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import se.academy.repository.DbRepository;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ShoppingCart {
 
-    @Autowired
-    private DbRepository repository;
-
     private List<Product> shoppinglist;
+    private Map<String,Integer> shoppingmap;
 
     public ShoppingCart(){
         this.shoppinglist = new ArrayList<>();
-
+        this.shoppingmap = new HashMap<>();
     }
 
     public List<Product> getShoppinglist() {
@@ -26,7 +23,22 @@ public class ShoppingCart {
         this.shoppinglist = shoppinglist;
     }
 
+    public Map<String, Integer> getShoppingmap() {
+        return shoppingmap;
+    }
+
+    public void setShoppingmap(Map<String, Integer> shoppingmap) {
+        this.shoppingmap = shoppingmap;
+    }
+
     public void addProduct(Product product){
         shoppinglist.add(product);
+        if(shoppingmap.get(product.getName()) == null){
+            shoppingmap.put(product.getName(),1);
+        }
+        else{
+            shoppingmap.put(product.getName(),shoppingmap.get(product.getName())+1);
+        }
     }
+
 }
