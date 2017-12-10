ded40045103275ce957d2658363c1aa5a04a2d64
Hasseoa <hasseoa@gmail.com>
2017-12-10 14:44:47 +0100

    removed unused shoppinglist

--- src/main/java/se/academy/domain/ShoppingCart.java
+++ src/main/java/se/academy/domain/ShoppingCart.java
@@ -1,26 +1,14 @@
 package se.academy.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ShoppingCart {
 
-    private List<Product> shoppinglist;
     private Map<String,Integer> shoppingmap;
 
     public ShoppingCart(){
-        this.shoppinglist = new ArrayList<>();
         this.shoppingmap = new HashMap<>();
-    }
-
-    public List<Product> getShoppinglist() {
-        return shoppinglist;
-    }
-
-    public void setShoppinglist(List<Product> shoppinglist) {
-        this.shoppinglist = shoppinglist;
     }
 
     public Map<String, Integer> getShoppingmap() {
@@ -32,7 +20,6 @@ public class ShoppingCart {
     }
 
     public void addProduct(Product product){
-        shoppinglist.add(product);
         if(shoppingmap.get(product.getName()) == null){
             shoppingmap.put(product.getName(),1);
         }
